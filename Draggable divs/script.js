d5bd15ed80c8a085193f50ea626c56572d8f7567
astraytel <113266307+astraytel@users.noreply.github.com>
2023-08-24 21:51:27 +0200

    Add files via upload

---
+++ Draggable divs/script.js
@@ -0,0 +1,48 @@
+gsap.registerPlugin(Flip, Draggable);
+const thumbnail = document.querySelector(".thumbnail");
+Draggable.create(thumbnail);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail");
+});
+
+const thumbnail2 = document.querySelector(".thumbnail2");
+Draggable.create(thumbnail2);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail2");
+});
+
+const thumbnail3 = document.querySelector(".thumbnail3");
+Draggable.create(thumbnail3);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail3");
+});
+
+const thumbnail4 = document.querySelector(".thumbnail4");
+Draggable.create(thumbnail4);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail4");
+});
+
+const thumbnail5 = document.querySelector(".thumbnail5");
+Draggable.create(thumbnail5);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail5");
+});
+
+const thumbnail6 = document.querySelector(".thumbnail6");
+Draggable.create(thumbnail6);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail6");
+});
+
+const thumbnail7 = document.querySelector(".thumbnail7");
+Draggable.create(thumbnail7);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail7");
+});
+
+const thumbnail8 = document.querySelector(".thumbnail8");
+Draggable.create(thumbnail8);
+document.addEventListener("click", () => {
+	const state = Flip.getState(".thumbnail8");
+});
